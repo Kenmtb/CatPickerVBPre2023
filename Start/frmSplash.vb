104f9d46dc3103bee466040cede38f5e7fd7ee71
Ken VanCamp <39036550+Kenmtb@users.noreply.github.com>
2022-06-03 13:57:38 -0400

    Add files via upload

---
+++ Start/frmSplash.vb
@@ -0,0 +1,27 @@
+﻿
+Imports Controlers
+Imports CatPickerVB
+
+Public Class frmSplash
+  Dim controller As CatController
+
+  Public Sub New()
+
+    ' This call is required by the designer.
+    InitializeComponent()
+    Me.WindowState = FormWindowState.Minimized
+
+    ' Add any initialization after the InitializeComponent() call.
+
+  End Sub
+
+
+
+
+  Private Sub Splash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    'controller = New CatController(New frmShowCats)
+    controller = New CatController()
+    controller.startForm()
+  End Sub
+
+End Class
